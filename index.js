8cf61e1b13572c7731f2a96118d659aee17b9809
Paloma <palomagutierrez947@gmail.com>
2024-02-14 14:47:44 +0100

    configuración de entorno

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 
 const servidor = express();
 
 
-servidor.listen(3000);
+servidor.listen(process.env.PORT);
+//para cuando lo subamos a internet 
+//en el que subamos a internet no hace falta el env
