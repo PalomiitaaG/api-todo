0106aa13cd0833ea9efa42e166a66f13de36be90
Paloma <palomagutierrez947@gmail.com>
2024-02-15 12:48:34 +0100

    endpoints basicos y entorno de pruebas

--- index.js
+++ index.js
@@ -3,6 +3,29 @@ const express = require("express");
 
 const servidor = express();
 
+servidor.use("/pruebas", express.static("./pruebas_api"));
+
+servidor.get("/api-todo", (peticion,respuesta) => {
+    respuesta.send("metodo GET");
+});
+
+servidor.post("/api-todo", (peticion,respuesta) => {
+    respuesta.send("metodo POST");
+});
+
+servidor.put("/api-todo", (peticion,respuesta) => {
+    respuesta.send("metodo PUT");
+});
+
+servidor.delete("/api-todo", (peticion,respuesta) => {
+    respuesta.send("metodo DELETE");
+});
+
+servidor.use("/", (peticion,respuesta) => {
+    respuesta.json({error : "not found"});
+});
+
+
 
 servidor.listen(process.env.PORT);
 //para cuando lo subamos a internet 
