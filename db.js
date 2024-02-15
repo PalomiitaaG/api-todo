f15b2d1e836d5261d98a7387982bbd60752cd295
Paloma <palomagutierrez947@gmail.com>
2024-02-15 14:54:36 +0100

    mitad de validacion de errores POST

---
+++ db.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const postgres = require("postgres");
+
+function conectar(){
+    return postgres({
+        host : process.env.DB_HOST,
+        database : process.env.DB_NAME,
+        user :  process.env.DB_USER,
+        password :  process.env.DB_PASSWORD
+    });
+}
+
+function getTareas(){
+    return new Promise(async (ok,ko) =>{ //2callback. 1 para cuando se cumpla y otra para cuando no
+        let conexion = conectar();
+
+        try{
+            let tareas = await conexion`SELECT * FROM tareas`; //para buscarlos en la base de datos
+
+            conexion.end();
+
+            ok(tareas); //cumplir la promesa pasando los colores.
+        }catch(error){
+            ko({error : "error en la base de datos"}); //esto seria un objeto
+        }
+
+    }); 
+}
+
+function crearTareas({tarea}){
+    return new Promise(async (ok,ko) =>{ //2callback. 1 para cuando se cumpla y otra para cuando no
+        let conexion = conectar();
+
+        try{
+            let [{id}] = await conexion`INSERT INTO tareas (tarea) VALUES (${tarea}) RETURNING id`; //para buscarlos en la base de datos
+
+            conexion.end();
+
+            ok(id); //cumplir la promesa pasando los colores.
+        }catch(error){
+            ko({error : "error en la base de datos"}); //esto seria un objeto
+        }
+
+    }); 
+}
+
+module.exports = {getTareas,crearTareas};
--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 require("dotenv").config();
 const express = require("express");
+const {getTareas,crearTareas} = require("./db");
+const {json} = require("body-parser");
 
 const servidor = express();
 
+servidor.use(json());
+
 servidor.use("/pruebas", express.static("./pruebas_api"));
 
-servidor.get("/api-todo", (peticion,respuesta) => {
-    respuesta.send("metodo GET");
+servidor.get("/api-todo", async (peticion,respuesta) => {
+    try{
+        let tareas = await getTareas();
+        respuesta.json(tareas);
+    }catch(error){
+        respuesta.status(500);
+        respuesta.json(error);
+    }
 });
 
-servidor.post("/api-todo", (peticion,respuesta) => {
-    respuesta.send("metodo POST");
+servidor.post("/api-todo/crear", async (peticion,respuesta,siguiente) => {
+    let {tarea} = peticion.body;
+    
+    if(tarea && tarea.trim() != ""){ //quierre decir que me han enviado almenos una letra.
+        return respuesta.send("metodo POST");
+    }
+
+    siguiente("..no me enviaste tareas"); //throw es como un return, es del catch te devuelve el error. No se puede hacer throw en una promesa asincona.
+    //siguiente te lleva directamente a servidor.use donde esta el error. 
+   
 });
 
 servidor.put("/api-todo", (peticion,respuesta) => {
@@ -25,6 +43,9 @@ servidor.use("/", (peticion,respuesta) => {
     respuesta.json({error : "not found"});
 });
 
+servidor.use((error,peticion,respuesta,siguiente) =>{ //express sabra que este se utilizara unicamente cuando haya un error. Por ejemplo en el caso de que coloquemos mal algo de los argumentos de string en el body. Solo hace esa funcion si hay 4 argummentos.
+    respuesta.send("..error");
+});
 
 
 servidor.listen(process.env.PORT);
